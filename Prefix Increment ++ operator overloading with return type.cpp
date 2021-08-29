c7aecce0451e7804a61678596e7b508a026f7e7a
RAHUL SHARMA <73328975+RAHULSHARMA63@users.noreply.github.com>
2021-08-29 17:17:13 +0530

    To print Prefix Increment ++ operator overloading with return type.

---
+++ Prefix Increment ++ operator overloading with return type.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+class Check
+{
+  private:
+    int i;
+  public:
+    Check(): i(0) {  }    
+    Check operator ++()
+    {
+       Check temp;
+       ++i;
+       temp.i = i;
+       return temp;
+    }
+    void Display()
+    { cout << "i = " << i << "\n"; }
+};
+int main()
+{
+    Check obj, obj1;
+    obj.Display();
+    obj1.Display();
+    obj1 = ++obj;
+    obj.Display();
+    obj1.Display();
+    return 0;
+}
